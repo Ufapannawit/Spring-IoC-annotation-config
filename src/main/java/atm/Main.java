8e73b17060b5de4d17536a3229339a115325ec55
Pannawit <pannawit.p@ku.th>
2022-07-24 04:36:43 +0700

    annotation config

---
+++ src/main/java/atm/Main.java
@@ -0,0 +1,22 @@
+package atm;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+
+        DataSourceFile dataSource = new DataSourceFile("customers.txt");
+//        Bank bankXYZ = new Bank("XYZ Bank", new DataSourceDB());
+//        Bank bankABC = new Bank("ABC Bank", new DataSourceFile("customersABC.txt"));
+//        ATM atm = new ATM(bankABC);
+
+
+        ApplicationContext context
+                = new ClassPathXmlApplicationContext("bean.xml");
+
+        AtmUI atmUI = context.getBean(AtmUI.class);
+        atmUI.run();
+    }
+}
